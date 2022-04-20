bf49a82ddd0ab4e7f50a510b92a3f1632f73127b
Evgen Daniletski <skifgtr@mail.ru>
2022-04-20 22:02:50 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { DeleteTwoTone, CloseSquareTwoTone, CheckCircleTwoTone } from '@ant-design/icons'
+import 'antd/dist/antd.css';
+import { List, Typography, Divider, Input, Button, } from 'antd';
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+
+<>
+    <Divider orientation="left"><h2>TODO list</h2></Divider>
+    <List
+      header={<div><h3>Количество активных задач {data.length} </h3></div>}
+      footer={<div>Добавьте новую задачу</div>}
+      bordered
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <Typography.Text mark>
+            < CloseSquareTwoTone />
+            < DeleteTwoTone />
+            <CheckCircleTwoTone twoToneColor="#eb2f96"/> 
+          </Typography.Text> {item} 
+        </List.Item>
+      )}
+    />
+        <Input.Group compact>
+      <Input style={{ width: 'calc(100% - 200px)' }} defaultValue="Новая задача" />
+      <Button type="primary">Submit</Button>
+    </Input.Group>
+  </></div>
   );
 }
 
