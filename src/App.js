43640b847f0ea74d0418c0a2ecd1993aff692c49
Evgen Daniletski <skifgtr@mail.ru>
2022-04-21 22:06:54 +0300

    add mockapi

--- src/App.js
+++ src/App.js
@@ -1,42 +1,79 @@
-
+import React from 'react'
 import { DeleteTwoTone, CloseSquareTwoTone, CheckCircleTwoTone } from '@ant-design/icons'
 import 'antd/dist/antd.css';
-import { List, Typography, Divider, Input, Button, } from 'antd';
-
-const data = [
-  'Racing car sprays burning fuel into crowd.',
-  'Japanese princess to wed commoner.',
-  'Australian walks 100km after outback crash.',
-  'Man charged over missing wedding girl.',
-  'Los Angeles battles huge wildfires.',
-];
+import { List, 
+  Typography, 
+  Divider, 
+} from 'antd';
+import axios from 'axios';
+import Demo from './components/Demo';
 
 function App() {
-  return (
-    <div>
+  const [todoList, setTodoList] = React.useState([])
 
-<>
-    <Divider orientation="left"><h2>TODO list</h2></Divider>
-    <List
-      header={<div><h3>Количество активных задач {data.length} </h3></div>}
-      footer={<div>Добавьте новую задачу</div>}
-      bordered
-      dataSource={data}
-      renderItem={item => (
-        <List.Item>
-          <Typography.Text mark>
-            < CloseSquareTwoTone />
-            < DeleteTwoTone />
-            <CheckCircleTwoTone twoToneColor="#eb2f96"/> 
-          </Typography.Text> {item} 
-        </List.Item>
-      )}
-    />
-        <Input.Group compact>
-      <Input style={{ width: 'calc(100% - 200px)' }} defaultValue="Новая задача" />
-      <Button type="primary">Submit</Button>
-    </Input.Group>
-  </></div>
+
+  React.useEffect(() => {
+        axios.get('https://62615c64327d3896e27a830c.mockapi.io/listTodo').then((function (response){setTodoList(response.data)}))
+
+  },[])
+
+const handleClickTask = async ({id, text, is_complete}) => {
+  await axios.put(`https://62615c64327d3896e27a830c.mockapi.io/listTodo/${id}`,{
+    "id": id,
+    "text": text,
+    "is_complete": !is_complete
+  })
+  await axios.get('https://62615c64327d3896e27a830c.mockapi.io/listTodo').then((function (response){setTodoList(response.data)}))
+}
+
+const handleDeleteTask = async (obj) => {
+  try {
+    console.log(obj.id);
+    await axios.delete(`https://62615c64327d3896e27a830c.mockapi.io/listTodo/${obj.id}`);
+    setTodoList((prev) => prev.filter(item => Number(item.id) !== Number(obj.id)));
+  } catch (error) {
+    alert('Ошибка при удалении задачи из списка');
+    console.error(error);
+  }
+}
+
+const handleAddTask = async (text) => {
+  console.log(text.text);
+    await axios.post(`https://62615c64327d3896e27a830c.mockapi.io/listTodo/`, {
+      "text": text.text,
+      "is_complete": false
+    })
+    await axios.get('https://62615c64327d3896e27a830c.mockapi.io/listTodo').then((function (response){setTodoList(response.data)}))
+  }
+
+
+return (
+    <div>
+    <>
+      <Divider orientation="left"><h2>Список задач</h2></Divider>
+      <List
+        header={<div><h3>Количество задач {todoList.length} </h3></div>}
+        footer={<div>Добавьте новую задачу</div>}
+        bordered
+        dataSource={todoList}
+        renderItem={item => (
+          <List.Item>
+            <Typography.Text mark onClick={()=>handleClickTask(item)} className="cu-p">
+            {item.is_complete 
+              ? <CheckCircleTwoTone twoToneColor="#eb2f96"/> 
+              : <CloseSquareTwoTone/>
+            }
+            </Typography.Text> 
+            <DeleteTwoTone 
+            onClick={()=>handleDeleteTask(item)} 
+            className="cu-p"
+            /> {item.text} 
+          </List.Item>
+        )}
+      />
+      <Demo addTask={handleAddTask} />
+    </>
+  </div>
   );
 }
 
---
+++ src/components/Demo.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Form, Input, message, Button, Space } from 'antd';
+
+const Demo = (
+     {addTask}
+      ) => {
+  const [form] = Form.useForm();
+
+  const onFinish = async (value) => {
+    message.success('Задача добавлена!');
+    await addTask(value);
+  };
+
+  const onFinishFailed = () => {
+    message.error('Ошибка добавления задачи!');
+  };
+
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+    <Form.Item
+        name="text"
+        label=""
+        rules={[
+        {
+            required: true,
+        },
+        {
+            type: 'string',
+            warningOnly: true,
+        },
+        {
+            type: 'string',
+            min: 6,
+        },
+        ]}
+    >
+        <Input placeholder="Введите задачу" allowClear="true" onPressEnter=""/>
+    </Form.Item>
+    <Form.Item>
+        <Space>
+          <Button type="primary" htmlType="submit">
+            Добавить
+          </Button>
+         </Space>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Demo;
