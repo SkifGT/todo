c68e868c1b34e2a47846630b5b6da0608b0c95a5
Evgen Daniletski <skifgtr@mail.ru>
2022-05-01 13:12:48 +0300

    Add .env

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React from 'react'
 import { DeleteTwoTone, CloseSquareTwoTone, CheckCircleTwoTone } from '@ant-design/icons'
 import 'antd/dist/antd.css';
-import { List, 
-  Typography, 
-  Divider, 
-} from 'antd';
+import { List, Typography,  Divider, } from 'antd';
 import axios from 'axios';
 import Demo from './components/Demo';
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 function App() {
   const [todoList, setTodoList] = React.useState([])
 
-
   React.useEffect(() => {
-        axios.get('https://62615c64327d3896e27a830c.mockapi.io/listTodo').then((function (response){setTodoList(response.data)}))
-
+        axios.get(BASE_URL).then((function (response){setTodoList(response.data)}))
   },[])
 
 const handleClickTask = async ({id, text, is_complete}) => {
-  await axios.put(`https://62615c64327d3896e27a830c.mockapi.io/listTodo/${id}`,{
+  await axios.put(`${BASE_URL}/${id}`,{
     "id": id,
     "text": text,
     "is_complete": !is_complete
   })
-  await axios.get('https://62615c64327d3896e27a830c.mockapi.io/listTodo').then((function (response){setTodoList(response.data)}))
+  const response = await axios.get(BASE_URL)
+  setTodoList(response.data)
 }
 
 const handleDeleteTask = async (obj) => {
   try {
-    console.log(obj.id);
-    await axios.delete(`https://62615c64327d3896e27a830c.mockapi.io/listTodo/${obj.id}`);
+    await axios.delete(`${BASE_URL}/${obj.id}`);
     setTodoList((prev) => prev.filter(item => Number(item.id) !== Number(obj.id)));
   } catch (error) {
     alert('Ошибка при удалении задачи из списка');
@@ -38,43 +35,41 @@ const handleDeleteTask = async (obj) => {
 }
 
 const handleAddTask = async (text) => {
-  console.log(text.text);
-    await axios.post(`https://62615c64327d3896e27a830c.mockapi.io/listTodo/`, {
-      "text": text.text,
-      "is_complete": false
-    })
-    await axios.get('https://62615c64327d3896e27a830c.mockapi.io/listTodo').then((function (response){setTodoList(response.data)}))
+  await axios.post(BASE_URL, {
+    "text": text.text,
+    "is_complete": false
+  })
+  const response = await axios.get(BASE_URL)
+  setTodoList(response.data)
   }
 
 
 return (
-    <div>
-    <>
-      <Divider orientation="left"><h2>Список задач</h2></Divider>
-      <List
-        header={<div><h3>Количество задач {todoList.length} </h3></div>}
-        footer={<div>Добавьте новую задачу</div>}
-        bordered
-        dataSource={todoList}
-        renderItem={item => (
-          <List.Item>
-            <Typography.Text  onClick={()=>handleClickTask(item)} className="cu-p">
-            {item.is_complete 
-              ? <CheckCircleTwoTone twoToneColor="#eb2f96" style={{fontSize: '18px', color: '#ffffff'}}/> 
-              : <CloseSquareTwoTone style={{fontSize: '18px'}}/>
-            }
-            </Typography.Text> 
-            <DeleteTwoTone 
-            onClick={()=>handleDeleteTask(item)} 
-            className="cu-p"
-            style={{fontSize: '18px'}}
-            /> {item.text} 
-          </List.Item>
-        )}
-      />
-      <Demo addTask={handleAddTask} />
-    </>
-  </div>
+  <>
+    <Divider orientation="left"><h2>Список задач</h2></Divider>
+    <List
+      header={<div><h3>Количество задач {todoList.length} </h3></div>}
+      footer={<div>Добавьте новую задачу</div>}
+      bordered
+      dataSource={todoList}
+      renderItem={item => (
+        <List.Item>
+          <Typography.Text  onClick={()=>handleClickTask(item)} className="cu-p">
+          {item.is_complete 
+            ? <CheckCircleTwoTone twoToneColor="#eb2f96" style={{fontSize: '18px', color: '#ffffff'}}/> 
+            : <CloseSquareTwoTone style={{fontSize: '18px'}}/>
+          }
+          </Typography.Text> 
+          <DeleteTwoTone 
+          onClick={()=>handleDeleteTask(item)} 
+          className="cu-p"
+          style={{fontSize: '18px'}}
+          /> {item.text} 
+        </List.Item>
+      )}
+    />
+    <Demo addTask={handleAddTask} />
+  </>
   );
 }
 
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Form, Input, message, Button, Space } from 'antd';
 
-const Demo = (
-      {addTask}
-      ) => {
+const Demo = ({addTask}) => {
   const [form] = Form.useForm();
 
   const onFinish = async (value) => {
     message.success('Задача добавлена!');
     form.resetFields();
     await addTask(value);
-    
   };
 
   const onFinishFailed = () => {
@@ -25,7 +22,7 @@ const Demo = (
       onFinishFailed={onFinishFailed}
       autoComplete="off"
     >
-    <Form.Item
+      <Form.Item
         name="text"
         label=""
         rules={[
@@ -41,15 +38,15 @@ const Demo = (
             min: 6,
         },
         ]}
-    >
-        <Input placeholder="Введите задачу" allowClear="true" onPressEnter=""/>
-    </Form.Item>
-    <Form.Item>
+      >
+      <Input placeholder="Введите задачу" allowClear="true" onPressEnter=""/>
+      </Form.Item>
+      <Form.Item>
         <Space>
           <Button type="primary" htmlType="submit">
             Добавить
           </Button>
-          </Space>
+        S</Space>
       </Form.Item>
     </Form>
   );
